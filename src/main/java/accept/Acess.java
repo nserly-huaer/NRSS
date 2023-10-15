41fd3730aad6a82465777ec9b9971646c00aad13
nserly <fakagsjf@outlook.com>
2023-10-15 13:32:29 +0800

    添加了对ipv6的支持

--- src/main/java/accept/Acess.java
+++ src/main/java/accept/Acess.java
@@ -4,9 +4,9 @@ import FileStart.Run;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,15 +17,18 @@ public class Acess {
     private static int clientCount = 0; // 当前客户端连接数量
     private static Logger logger = LogManager.getLogger(Acess.class);
     private ExecutorService executorService;
+    public static int ServerPort;
 
     public void access(int ServerPort, int MaxConnect) {
+        Acess.ServerPort = ServerPort;
         executorService = Executors.newFixedThreadPool(MaxConnect);
         try {
             // 创建服务器套接字，监听指定端口
             ServerSocket serverSocket = new ServerSocket(ServerPort);
             InetAddress localhost = InetAddress.getLocalHost();
             String ipAddress = localhost.getHostAddress();
-            System.out.println("服务器信息：\nip: " + ipAddress + ":" + ServerPort);
+            System.out.print("服务器信息：\nipv4: " + ipAddress + ":" + ServerPort);
+            System.out.println(GetIPv6.getIPv6());
             logger.info("服务器信息：\nip:" + ipAddress + ":" + ServerPort);
             System.out.println("\n\n服务器已启动，等待客户端连接...");
             logger.info("服务器已启动，等待客户端连接...");
@@ -43,7 +46,7 @@ public class Acess {
                 clientCount++;
                 executorService.execute(new ClientHandler(clientSocket));
                 System.out.println("客户端连接成功，IP地址：" + clientSocket.getInetAddress().getHostAddress());
-                logger.info("客户端连接成功，IP地址：" + clientSocket.getInetAddress().getHostAddress());
+                logger.info("客户端连接成功，IPv4地址：" + clientSocket.getInetAddress().getHostAddress());
             }
 
         } catch (IOException e) {
